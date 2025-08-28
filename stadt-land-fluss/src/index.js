6be8c77cd2b6d23ac9e9afce775a9bfaadee0a4e
emi-hub-byte <emilialohmann03@gmail.com>
2025-08-28 12:43:39 +0200

    haha hihi Hello World

--- stadt-land-fluss/src/index.js
+++ stadt-land-fluss/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client";
+import { Test } from "./View/home.js";
 
-const element = <h1>Hello World</h1>;
-
-ReactDom.render(element, document.getElementById("root"));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Test />);
