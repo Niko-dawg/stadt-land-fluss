7c00303c3bad234b39ccdcf9ef3ff07e4dc2b381
emi-hub-byte <emilialohmann03@gmail.com>
2025-08-31 15:39:30 +0200

    spiel grundlage

--- stadt-land-fluss/src/View/spiel.js
+++ stadt-land-fluss/src/View/spiel.js
@@ -2,25 +2,48 @@ import React from "react";
 import "./spiel.css";
 
 export class Spiel extends React.Component {
-/*
-    handleSinglePlayer = () => {
-    alert("Single Player ausgewählt!");
-  };
-
-  handleMultiPlayer = () => {
-    alert("Multiplayer ausgewählt!");
-  };
-*/
   render() {
     return (
-        <div>Hello World</div>
-     /*
-     <div style={{ backgroundColor: "#fcf8ed", minHeight: "100vh", padding: "10px" }}>
-        <Header />
-        <GameMode title="Single Player" onClick={this.handleSinglePlayer} />
-        <GameMode title="Multiplayer" players={4} onClick={this.handleMultiPlayer} />
+      <div className="container">
+        <div className="header">
+          <div className="logo">SLF!</div>
+          <div className="timer">60</div>
+        </div>
+
+        <div className="letter">
+          <p>Der Gesuchte Buchstabe ist :</p>
+          <div className="big-letter">A</div>
+        </div>
+
+        <div className="game-grid">
+          <div className="input-row">
+            <div className="background-image bg-1"></div>
+            <label>Stadt</label>
+            <input type="text" defaultValue="Aachen" />
+          </div>
+          <div className="input-row">
+            <div className="background-image bg-2"></div>
+            <label>Land</label>
+            <input type="text" defaultValue="Australien" />
+          </div>
+          <div className="input-row">
+            <div className="background-image bg-3"></div>
+            <label>Fluss</label>
+            <input type="text" defaultValue="Ahr" />
+          </div>
+          <div className="input-row">
+            <div className="background-image bg-4"></div>
+            <label>Tier</label>
+            <input type="text" defaultValue="Affe" />
+          </div>
+        </div>
+
+        <button>Antworten Abgeben</button>
+
+        <div className="players-finished">
+          fertige Spieler <span>1/3</span>
+        </div>
       </div>
-      */
     );
   }
 }
